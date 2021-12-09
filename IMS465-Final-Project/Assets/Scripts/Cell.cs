b1496b63b602a464884f0ee096712e35ea0b3aee
trotmacl <trotmacl@miamioh.edu>
2021-12-09 02:21:15 -0500

    Adds coordinates for enemies and players

---
+++ IMS465-Final-Project/Assets/Scripts/Cell.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+
+    [SerializeField] public bool isSpotlight;
+    [SerializeField] public float enemyX;
+    [SerializeField] public float enemyZ;
+    [SerializeField] public float playerX;
+    [SerializeField] public float playerZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
